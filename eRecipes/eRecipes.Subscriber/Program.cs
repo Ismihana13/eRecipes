e986a179bb599aa5c587b38cfd463427cd4ff1a0
Ismihana <ismihana.medjedovic@edu.fit.ba>
2024-10-25 10:54:52 +0200

    rabbitmq

--- eRecipes/eRecipes.Subscriber/Program.cs
+++ eRecipes/eRecipes.Subscriber/Program.cs
@@ -11,6 +11,17 @@ await bus.PubSub.SubscribeAsync<ReceptActivated>("console_printer", msg =>
     Console.WriteLine($"Recipes activated: {msg.Recept.Naziv}");
 });
 
+await bus.PubSub.SubscribeAsync<ReceptActivated>("console_printer", msg =>
+{
+    Console.WriteLine($"Recipes activated 2: {msg.Recept.Naziv}");
+});
+
+await bus.PubSub.SubscribeAsync<ReceptActivated>("mail_sender", msg =>
+{
+    Console.WriteLine($"Sending email for: {msg.Recept.Naziv}");
+    //to do send email
+});
+
 Console.WriteLine("Listening for messages, press <return> key to close!");
 Console.ReadLine();
 
